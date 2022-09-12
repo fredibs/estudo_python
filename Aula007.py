0fd785cbc37cdf1b136002c44fd757549eb86c34
fredibs <111262819+fredibs@users.noreply.github.com>
2022-09-11 22:24:15 -0300

    Create Ex-005.py

---
+++ Aula007.py
@@ -0,0 +1,13 @@
+print('=============== AULA 007 ===============')
+n1 = int(input('Um Valor:'))
+n2 = int(input('Outro Valor:'))
+s = n1 + n2
+m = n1 * n2 
+d = n1 / n2 
+di = n1 // n2 
+ex = n1 ** n2 
+print('A soma é: {}, \n O produto é: {}, \n e a divisão é: {:.3f}' .format(s, m ,d), end=' ')
+print('\n Divisão inteira e: {} \n e a potência é {}' .format(di, ex))
+print('')
+print('============ FIM DA AULA 007 ============')
+print('')
