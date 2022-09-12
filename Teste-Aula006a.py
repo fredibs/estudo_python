36415f4652a3da7e498b08b3a144c3dd2f4aa6a5
fredibs <111262819+fredibs@users.noreply.github.com>
2022-09-11 22:23:27 -0300

    Create Teste-Aula006a.py

---
+++ Teste-Aula006a.py
@@ -0,0 +1,37 @@
+print('')
+print('============= AULA 006 =============')
+print('TIPOS PRIMITIVOS')
+print('PRINCIPAIS TIPOS: int float boll str')
+#print('')
+#n1 = input('Digite o primeiro número: ')
+#print(type(n1))
+#print('')
+#n1 = int(input('Digite o primeiro número: '))
+#print(type(n1))
+#print('')
+
+#print('')
+#n1 = input('Digite o primeiro número: ')
+#n2 = input('Digite o segundo número: ')
+#sn = n1+n2
+#print('A soma dos valores é', sn)
+#print('')
+
+#print('')
+#n1 = int(input('Digite o primeiro número: '))
+#n2 = int(input('Digite o segundo número: '))
+#sn = n1+n2
+#print('A soma dos valores é', sn)
+#print('')
+
+print('')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+sn = n1+n2
+#Minha solução para o problema
+#print('A soma dos números', n1, 'e', n2, 'vale:', sn)
+#Solução do professor
+print('A soma dos números {} e {} vale: {}'.format(n1, n2, sn))
+print('')
+
+print('========= FIM DA AULA 006 ==========')
