9a83fb41b1c2c9b9e01bcff8bf723f42ec7a43c8
fredibs <111262819+fredibs@users.noreply.github.com>
2022-09-11 22:23:35 -0300

    Create Teste-Aula006b.py

---
+++ Teste-Aula006b.py
@@ -0,0 +1,11 @@
+#n = float(input('Digite um valor: '))
+#print(n)
+
+#n = str(input('Digite um valor: '))
+#print(type(n))
+
+#n = bool(input('Digite um valor: '))
+#print(type(n))
+
+n = bool(input('Digite um valor: '))
+print(n)
